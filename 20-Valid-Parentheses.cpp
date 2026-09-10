069881c5f004e530fec3c59a4f598321ffea802d
thphatts <ngthanhphat0812@gmail.com>
2026-09-10 16:25:25 +0700

    Add 20-Valid-Parentheses solution

---
+++ 20-Valid-Parentheses.cpp
@@ -0,0 +1,37 @@
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+1.Open brackets must be closed by the same type of brackets.
+2.Open brackets must be closed in the correct order.
+3.Every close bracket has a corresponding open bracket of the same type.
+*/
+#include <iostream>
+using namespace std;
+bool isValid(string s)
+{
+    stack<char> st;
+    for (char c : s)
+    {
+        if (c == '(' || c == '[' || c == '{')
+        {
+            st.push(c);
+        }
+        else
+        {
+            if (st.empty())
+            {
+                return false;
+            }
+            char top = st.top();
+            if ((c == ')' && top == '(') || (c == '}' && top == '{') || (c == ']' && top == '['))
+            {
+                st.pop();
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    return st.empty();
+}
